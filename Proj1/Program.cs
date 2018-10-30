9c0267e73e4733ea4dccdd6168c4efa1f8aac18c
murad masarwi <masarwi63@gmail.com>
2018-10-30 22:12:14 +0200

    SolProj1

---
+++ Proj1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Proj1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int i = 2;
+//            int num = int.Parse(Console.ReadLine());
+//          while(num>0)
+//            {
+
+//                if(num%i==0)
+//                {
+//                    Console.WriteLine(i);
+//                }
+//                else if(i>10)
+//                {
+//                    break;
+//                }
+//                i++;
+//            }
+//        }
+//    }
+//}
+
+//namespace Proj1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num = int.Parse(Console.ReadLine());
+//            int max = 1;
+//            while (num >= max)
+//            {
+//                if (max % 7 == 0)
+//                {
+//                    Console.WriteLine("7Boom");
+
+//                }
+//                else
+//                {
+//                    Console.WriteLine(max);
+//                }
+
+//                max++;
+//            }
+
+//        }
+//    }
+//}
+
+
+//namespace Proj1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            string Done;
+//            int count = 0;
+//            int sum = 0;
+//            Console.WriteLine("enter a numbers if you finished write Done!");
+//            do
+//            {
+
+              
+//                int num = int.Parse(Console.ReadLine());
+//                Done = Console.ReadLine();
+//                sum += num;
+//                count++;
+//            } while (Done != "done");
+//            int avg;
+//            avg = sum / count;
+//            Console.WriteLine(avg);
+
+
+//        }
+
+//    }
+//}
